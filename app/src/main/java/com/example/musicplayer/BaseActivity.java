98c7db379b94535be9b4c6d88e80fbb22ab9ef84
zwy2021 <851941068@qq.com>
2021-06-10 15:28:51 +0800

    first commit

---
+++ app/src/main/java/com/example/musicplayer/BaseActivity.java
@@ -0,0 +1,45 @@
+package com.example.musicplayer;
+
+import android.app.Activity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.IdRes;
+
+//描述Activity类的共性，是项目中所有Activity类的父类
+public class BaseActivity extends Activity {
+    private ImageView mIvBack,mIvMe;
+    private TextView mTvTitle;
+
+    //使用泛型，规定上限为View
+    protected <T extends View> T fd(@IdRes int id){
+        return findViewById(id);
+    }
+
+    /**
+     *
+     *
+     *
+     * @param isShowBack:back按钮显示与否
+     * @param title:页面名称
+     * @param isShowMe 最右侧的“我的”图标显示与否
+     */
+    protected void initNavBar(boolean isShowBack,String title,boolean isShowMe){
+        mIvBack=findViewById(R.id.iv_back);
+        mTvTitle =findViewById(R.id.tv_title);
+        mIvMe=findViewById(R.id.rv_me);
+
+        mIvBack.setVisibility(isShowBack? View.VISIBLE:View.GONE);
+        mIvMe.setVisibility(isShowMe? View.VISIBLE:View.GONE);
+        mTvTitle.setText(title);
+
+        mIvBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/musicplayer/MyApplication.java
@@ -0,0 +1,11 @@
+package com.example.musicplayer;
+
+import android.app.Application;
+//存储系统信息
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
